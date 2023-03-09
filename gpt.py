83df8ce26dc74dfea0c8af50055b2aa611d8f99c
Abhishek Sengupta <106603514+silverfruitplayer@users.noreply.github.com>
2023-03-08 23:36:52 -0800

    yeah added

--- gpt.py
+++ gpt.py
@@ -22,7 +22,7 @@ app = Client("test1", bot_token="5808857616:AAEhIze6UIe-BnN8_S9Iu5tIT2owsBTOJdA"
 
 @app.on_message(filters.command("start"))
 async def start(_, message):
-    await message.reply_text(f"Hi {message.from_user.mention}, Ask any question to start over.")
+    await message.reply_text(f"Hi {message.from_user.mention}, Ask any question to start over.\nYou can search images too (NSFW content not allowed)")
     
 
 @app.on_message(filters.text)
@@ -73,7 +73,52 @@ async def message_handler(_, message):
         else:
             await message.reply(response["choices"][0]["text"])
             await generating_message.delete()
-        
+            
+@app.on_message(filters.text)
+async def message_handler(_, message):
+    if message.text:
+        generating_message = await message.reply("Generating response...")
+
+        message_text = message.text    
+
+        while True:
+            try:
+                response1 = openai.Image.create(
+                   prompt=f"{message_text}\n",
+                   n=2,
+                   size="1024x1024"
+                )
+                break   
+                
+            except openai.error.Timeout as e:
+                await message.reply(f"The what!?\n!!error start!!\n{e}\n!!!error end!!!")
+                break
+            
+            except openai.error.RateLimitError as e:
+                print(f"OpenAI API request exceeded rate limit: {e}")
+                break
+            
+            except openai.error.InvalidRequestError as e:
+                await message.reply(f"The what!?\n!!error start!!\n{e}\n!!!error end!!!")
+                break                                      
+                
+        if len(response1) > 4096:
+            filename = "sex.txt"
+            evaluation = "Success"
+
+            with open(filename, "w+", encoding="utf8") as out_file:
+                out_file.write(str(evaluation.strip()))
+                await message.reply_document(
+                    document=filename,
+                    caption="ok",
+                    disable_notification=True,
+                    reply_to_message_id=reply_to_message.message.id,
+                )
+                os.remove(filename)
+        else:
+            x =  response1["data"][0]["url"]
+            await message.reply(x)
+            await generating_message.delete()        
              
 app.start()
 idle()     
